4e7f6b84d43556da54f3ceca9d3e2c7ec966aa04
quentincornier <quentincornier95@gmail.com>
2024-03-30 16:32:13 +0100

    Commenting the grid_evalutation functions

--- sudokuMaker/grid_validation/grid_validation.cpp
+++ sudokuMaker/grid_validation/grid_validation.cpp
@@ -17,12 +17,12 @@ bool grid_validation::is_valid(std::array<int, 81> grid) {
 
 int grid_validation::get_index(int x, int y) {
     
-    return y * 9 + x;
+    return y * 9 + x;  // 9x9 grid with 0 <= x, y <= 8
 }
 
 bool grid_validation::is_row_valid(std::array<int, 81> grid, int row_index) {
 
-    std::array<int, 9> row;
+    std::array<int, 9> row;  // creating an array to store only the necessary number
     int row_starting_index = get_index(0, row_index);
 
     for (int k = 0; k < 9; k++) {  // Stores the row in an array
@@ -31,16 +31,16 @@ bool grid_validation::is_row_valid(std::array<int, 81> grid, int row_index) {
 
     int sum_of_row {0};
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 9; i++) {  // Sums all the number of the array
         sum_of_row += row[i];
     }
 
-    return sum_of_row == SUM_TO_9;
+    return sum_of_row == SUM_TO_9;  // the sum must be equal to 1 + 2 + ... + 9 in order to be correct
 }
 
 bool grid_validation::is_column_valid(std::array<int, 81> grid, int column_index) {
 
-    std::array<int, 9> column;
+    std::array<int, 9> column;  // creating an array to store only the necessary number
     int column_starting_index = get_index(column_index, 0);
 
     for (int k = 0; k < 9; k++) {  // Stores the column in an array
@@ -49,16 +49,16 @@ bool grid_validation::is_column_valid(std::array<int, 81> grid, int column_index
 
     int sum_of_column {0};
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 9; i++) {  // Sums all the number of the array
         sum_of_column += column[i];
     }
 
-    return sum_of_column == SUM_TO_9;
+    return sum_of_column == SUM_TO_9;  // the sum must be equal to 1 + 2 + ... + 9 in order to be correct
 }
 
 bool grid_validation::is_square_valid(std::array<int, 81> grid, int square_index) {
 
-    std::array<int, 9> square;
+    std::array<int, 9> square;  // creating an array to store only the necessary number
     int square_starting_x = square_index % 3 * 3;
     int square_starting_y = (square_index / 3) * 3;
 
@@ -69,7 +69,7 @@ bool grid_validation::is_square_valid(std::array<int, 81> grid, int square_index
     I multiple these index by 3 in order to have the coordinate of the smaller 9x9 grid
     */  
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) {  // stores the square in an array
         for (int j = 0; j < 3; j++) {
             square[i * 3 + j] = get_index(square_starting_x + j, square_starting_y + i);
         }
@@ -84,10 +84,9 @@ bool grid_validation::is_square_valid(std::array<int, 81> grid, int square_index
 
    int sum_of_square {0};
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 9; i++) {  // Sums all the number of the array
         sum_of_square += square[i];
     }
 
-    return sum_of_square == SUM_TO_9;
-
+    return sum_of_square == SUM_TO_9;  // the sum must be equal to 1 + 2 + ... + 9 in order to be correct
 }
--- sudokuMaker/grid_validation/grid_validation.h
+++ sudokuMaker/grid_validation/grid_validation.h
@@ -1,5 +1,5 @@
 /*
-For a giving grid, verifies if it respect the rules of sudoku.
+For a given grid, verifies if it respect the rules of sudoku.
 
 Take array of 81 numbers and return true if it is valid, else false.
 */
@@ -9,11 +9,54 @@ Take array of 81 numbers and return true if it is valid, else false.
 
 namespace grid_validation {
 
+    /*****************************************************************************
+       * For a given grid, verifies if it respect the rules of sudoku.
+       * 
+       * @param grid an array of integers which represents the sudoku
+       * 
+       * @return a boolean, true if the grid is valid, else false
+    *****************************************************************************/
     bool is_valid (std::array<int, 81> grid);
 
+    /*****************************************************************************
+       * For a given row, verifies if it respect the rules of sudoku.
+       * 
+       * @param grid an array of integers which represents the sudoku
+       * @param row_index an interger that represents the index of the row starting from the top
+       * 
+       * @return a boolean, true if the row is valid, else false
+    *****************************************************************************/
     bool is_row_valid(std::array<int, 81> grid, int row_index);
+
+    /*****************************************************************************
+       * For a given column, verifies if it respect the rules of sudoku.
+       * 
+       * @param grid an array of integers which represents the sudoku
+       * @param column_index an interger that represents the index of the column starting from the left
+       * 
+       * @return a boolean, true if the grid is column, else false
+    *****************************************************************************/
     bool is_column_valid(std::array<int, 81> grid, int column_index);
+
+    /*****************************************************************************
+       * For a given square a sudoku, verifies if it respect the rules of sudoku.
+       * 
+       * @param grid an array of integers which represents the sudoku
+       * @param square_index an interger that represents the index of the square starting from 
+       * the top right corner to the bottom left corner
+       * 
+       * @return a boolean, true if the square is valid, else false
+    *****************************************************************************/
     bool is_square_valid(std::array<int, 81> grid, int square_index);
 
+    /*****************************************************************************
+       * A sudoku is represented as an array of 81 integers and this function translate the coordinate
+       * of the grid representation to the index in the vectorial one.
+       * 
+       * @param x an integer which represents the abscisse 
+       * @param y an integer which represents the ordinate
+       * 
+       * @return return the index in the vector representation
+    *****************************************************************************/
     int get_index(int x, int y);
 }
